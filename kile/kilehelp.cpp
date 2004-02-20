36c3ec8864d37ebf7d05de00290fcf1a380f1dc7
Jeroen Wijnhout <wijnhout@science.uva.nl>
2004-02-20 17:48:57 +0000

    forgot to add these

---
+++ kile/kilehelp.cpp
@@ -0,0 +1,206 @@
+/***************************************************************************
+                           kilehelp.cpp
+----------------------------------------------------------------------------
+    date                 : Feb 09 2004
+    version              : 0.10.0
+    copyright            : (C) 2004 by Holger Danielsson
+    email                : holger.danielsson@t-online.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include <qfile.h>
+#include <qtextstream.h>
+#include <qregexp.h>
+#include <qfileinfo.h>
+
+#include <kapplication.h>
+#include <kglobal.h>
+#include <kstddirs.h>
+#include <kdebug.h>
+#include <klocale.h>
+#include <kate/document.h>
+
+#include "kilehelp.h"
+#include "kilestdtools.h"
+
+namespace KileHelp
+{
+
+	Help::Help(KileTool::Manager *manager,KileEdit *edit)
+		: m_manager(manager), m_edit(edit)
+	{
+		m_config = kapp->config();
+		//FIXME: make this configurable asap
+		m_texmfDir = "/usr/share/texmf/doc";
+
+		readHelpList("latex-kile.lst",m_dictHelpKile);
+		readHelpList("latex-tetex.lst",m_dictHelpTetex);
+	}
+	
+	////////////////////// show help //////////////////////
+	
+	void Help::showHelpFile(const QString &parameter)
+	{
+		KileTool::View *tool = new KileTool::View("ViewHTML", m_manager);
+		tool->setFlags(0);
+		tool->setSource(parameter);
+		tool->setTarget(QFileInfo(parameter).fileName());
+		m_manager->run(tool);
+	}
+
+////////////////////// Help: TeTeX //////////////////////
+
+	void Help::helpTetex(KileHelp::Type type)
+	{
+		QString filename;
+
+		switch ( type )
+		{
+			case HelpTetexGuide:
+				filename = "newhelpindex.html";
+				break;
+			case HelpTetexDoc:
+				filename = "helpindex.html";
+				break;
+			default:
+				return;
+		}
+
+		showHelpFile( m_texmfDir + "/" + filename );
+	}
+
+////////////////////// Help: LaTeX //////////////////////
+
+	void Help::helpLatex(Type type)
+	{
+		QString subject = "";
+
+		switch ( type )
+		{
+			case HelpLatexIndex:
+				break;
+			case HelpLatexCommand:
+				subject = "#cmd";
+				break;
+			case HelpLatexSubject:
+				subject = "#subj";
+				break;
+			case HelpLatexEnvironment:
+				subject = "#env";
+				break;
+			default:
+				break;
+		}
+
+		kdDebug() << "subject " << subject << endl;
+		showHelpFile( m_texmfDir + "/latex/latex2e-html/ltx-2.html" + subject );
+	}
+
+	////////////////////// Help: Keyword //////////////////////
+
+	void Help::helpKeyword(Kate::View *view)
+	{
+		//FIXME: read this variable from config file
+		int type = HelpLatex;
+		switch ( type )
+		{
+		case HelpTetex:
+			kdDebug() << "!ok" << endl;
+			helpTetexKeyword(view);
+			break;
+		case HelpLatex:
+			kdDebug() << "ok" << endl;
+			helpLatexKeyword(view);
+			break;
+		default:
+			kdDebug() << "!ok" << endl;
+			break;
+		}
+		kdDebug() << "!!!ok" << endl;
+	}
+
+	void Help::helpTetexKeyword(Kate::View *view)
+	{
+		QString word = getKeyword(view);
+		kdDebug() << "keyword: " << word << endl;
+		if ( !word.isNull() && m_dictHelpTetex.contains(word) )
+		{
+			kdDebug() << "about to show help for " << word << " (section " << m_dictHelpTetex[word] << " )" << endl;
+			showHelpFile( m_texmfDir + "/latex/latex2e-html/" + m_dictHelpTetex[word] );
+		}
+	}
+
+	void Help::helpLatexKeyword(Kate::View *view)
+	{
+		QString kilehelp = KGlobal::dirs()->findResource("html","en/kile/latexhelp.html");
+		kdDebug() << "kilehelp = " << kilehelp << endl;
+		if ( ! kilehelp.isEmpty() )
+		{
+			QString word = getKeyword(view);
+			kdDebug() << "word = " << word << " " << m_dictHelpKile.contains(word) << endl;
+			if ( !word.isNull() && m_dictHelpKile.contains(word) )
+			{
+				showHelpFile( kilehelp + "#" + m_dictHelpKile[word] );
+			}
+		}
+	}
+
+	QString Help::getKeyword(Kate::View *view)
+	{
+		if ( !view )
+			return QString::null;
+		
+		// get current position
+		uint row,col,col1,col2;
+		QString word;
+		Kate::Document *doc = view->getDoc();
+		view->cursorPositionReal(&row,&col);
+		
+		if ( m_edit->getCurrentWord(doc,row,col,KileEdit::smTex,word,col1,col2) )
+			return word;
+		else
+			return QString::null;
+	}
+
+//////////////////// read help lists ////////////////////
+
+	void Help::readHelpList(const QString &filename,QMap<QString,QString> &map)
+	{
+		QString file = KGlobal::dirs()->findResource("appdata","help/" + filename);
+		if ( file.isEmpty() )
+		{
+			kdDebug() << "   file not found: " << filename << endl;
+			return;
+		}
+
+		kdDebug() << "   read file: " << filename << endl;
+		QRegExp reg("\\s*(\\S+)\\s*=>\\s*(\\S+)");
+
+		QFile f(file);
+		if ( f.open(IO_ReadOnly) )
+		{     // file opened successfully
+			QTextStream t( &f );         // use a text stream
+			while ( ! t.eof() )
+			{        // until end of file...
+			QString s = t.readLine().stripWhiteSpace();       // line of text excluding '\n'
+			if ( ! (s.isEmpty() || s.at(0)=='#') )
+			{
+				int pos = reg.search(s);
+				if ( pos != -1 )
+				{
+				map[reg.cap(1)] = reg.cap(2);
+				}
+			}
+			}
+			f.close();
+		}
+	}
+}
---
+++ kile/kilehelp.h
@@ -0,0 +1,82 @@
+/***************************************************************************
+                           kilehelp.h
+----------------------------------------------------------------------------
+    date                 : Feb 09 2004
+    version              : 0.10.0
+    copyright            : (C) 2004 by Holger Danielsson
+    email                : holger.danielsson@t-online.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef KILEHELP_H
+#define KILEHELP_H
+
+#include <kconfig.h>
+#include <kate/view.h>
+
+#include <qmap.h>
+#include <qstring.h>
+
+#include <kileedit.h>
+#include "usermenudialog.h"
+#include "kiletool.h"
+#include "kiletoolmanager.h"
+
+/**
+  *@author Holger Danielsson
+  */
+
+namespace KileHelp
+{
+
+	enum Type 
+	{
+		HelpTetex,
+		HelpTetexGuide,
+		HelpTetexDoc,
+		HelpLatex,
+		HelpLatexIndex,
+		HelpLatexCommand,
+		HelpLatexSubject,
+		HelpLatexEnvironment
+	};
+
+	class Help
+	{
+	public:
+		Help(KileTool::Manager *manager,KileEdit *edit);
+		~Help()
+		{}
+		
+		// calls for help
+		void helpLatex(KileHelp::Type type);
+		void helpTetex(KileHelp::Type type);
+		void helpKeyword(Kate::View *view);
+
+	private:
+		KileTool::Manager *m_manager;
+		KConfig *m_config;
+		KileEdit *m_edit;
+		
+		QString m_texmfDir;
+		QMap<QString,QString> m_dictHelpKile;
+		QMap<QString,QString> m_dictHelpTetex;
+
+		void readHelpList(const QString &filename,QMap<QString,QString> &map);
+		void showHelpFile(const QString &parameter);
+
+		void helpTetexKeyword(Kate::View *view);
+		void helpLatexKeyword(Kate::View *view);
+		QString getKeyword(Kate::View *view);
+	};
+}
+
+#endif

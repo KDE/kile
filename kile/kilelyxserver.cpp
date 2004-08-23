af58009448375d46fa049afdb624345d4a8bfdb3
Jeroen Wijnhout <wijnhout@science.uva.nl>
2004-08-23 09:19:55 +0000

    revert changes, is giving 100% for some systems (Fedora at least)

--- kile/kilelyxserver.cpp
+++ kile/kilelyxserver.cpp
@@ -18,10 +18,6 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <stdlib.h> //getenv
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/time.h>
 
 #include "kilelyxserver.h"
 #include "kileactions.h"
@@ -40,15 +36,12 @@
 #include <kurl.h>
 
 KileLyxServer::KileLyxServer(bool st) :
-	m_running(false)
+	m_running(false),
+	m_count(0)
 {
-	m_pipeFile.setAutoDelete(true);
+	m_pipeIn.setAutoDelete(true);
+	m_notifier.setAutoDelete(true);
 
-	QString home(getenv("HOME"));
-	//in pipes should go first
-	m_pipes << home+"/.lyxpipe.in" << home+"/.lyx/lyxpipe.in";
-	m_pipes << home+"/.lyxpipe.out" << home+"/.lyx/lyxpipe.out";
-	
 	if (st)
 		start();
 }
@@ -56,7 +49,6 @@ KileLyxServer::KileLyxServer(bool st) :
 KileLyxServer::~KileLyxServer()
 {
 	stop();
-	//TODO remove pipes here, otherwise LyX is confused, and we don't want that
 }
 
 bool KileLyxServer::start()
@@ -66,129 +58,115 @@ bool KileLyxServer::start()
 
 	kdDebug() << "starting the LyX server..." << endl;
 
-	createPipes();
-	openPipes();
-	
-	m_running=true;
-	m_thread = new LyxPipeThread(this, m_fds);
-	m_thread->start();
+	if (openPipes())
+	{
+		QSocketNotifier *notifier;
+		QPtrListIterator<QFile> it(m_pipeIn);
+		while (it.current())
+		{
+			if ((*it)->name().right(3) == ".in" )
+			{
+				notifier = new QSocketNotifier((*it)->handle(), QSocketNotifier::Read, this);
+				connect(notifier, SIGNAL(activated(int)), this, SLOT(receive(int)));
+				m_notifier.append(notifier);
+				kdDebug() << "Created notifier for " << (*it)->name() << endl;
+			}
+			else
+				kdDebug() << "No notifier created fro " << (*it)->name() << endl;
+			++it;
+		}
+		m_running=true;
+	}
 
 	return m_running;
 }
 
+bool KileLyxServer::openPipes()
+{
+	QString home(getenv("HOME"));
+
+	QStringList pipes;
+	pipes << home+"/.lyxpipe.in" << home+"/.lyx/lyxpipe.in";
+	pipes << home+"/.lyxpipe.out" << home+"/.lyx/lyxpipe.out";
+
+	bool opened = false;
+	QFileInfo info;
+	QFile *file;
+
+	for (uint i=0; i < pipes.count(); i++)
+	{
+		info.setFile(pipes[i]);
+		if ( ! info.exists() )
+		{
+			mode_t perms = S_IRUSR | S_IWUSR | S_IRGRP| S_IROTH;
+			//create the dir first
+			if (mkdir(info.dirPath().ascii(), perms | S_IXUSR) == -1)
+				perror( "Could not create directory for pipe ");
+			else
+				kdDebug() << "Created directory " << info.dirPath() << endl;
+
+			if (mkfifo(pipes[i].ascii(), perms) == -1)
+   				perror( "Could not create pipe ");
+			else
+				kdDebug() << "Created pipe " << pipes[i] << endl;
+		}
+
+		file  = new QFile(info.absFilePath());
+		if (!file->open(IO_ReadWrite))
+		{
+			kdError() << "Could not open " << info.absFilePath() << endl;
+		}
+		else
+		{
+			kdDebug() << "Opened " << info.absFilePath() << endl;
+			m_pipeIn.append(file);
+			m_file.insert(file->handle(),file);
+			opened=true;
+
+			//read all data on the pipe (this is to ensure that old unprocessed data is not going to bother us)
+			//file->readAll(); //find a non-blocking solution
+		}
+	}
+
+	return opened;
+}
+
 void KileLyxServer::stop()
 {
-	QPtrListIterator<QFile> it(m_pipeFile);
+	kdDebug() << "stopping the LyX server after " << m_count << " requests..." << endl;
+
+	QPtrListIterator<QFile> it(m_pipeIn);
 	while (it.current())
 	{
 		(*it)->close();
 		++it;
 	}
-	m_pipeFile.clear();
+
+	m_pipeIn.clear();
+	m_notifier.clear();
+
 	m_running=false;
 }
 
-void KileLyxServer::createPipes()
+void KileLyxServer::receive(int fd)
 {
-	for (uint i = 0; i < m_pipes.count(); i++)
+	kdDebug() << "==KileLyxServer::receive(" << fd << ")==============" << endl;
+	m_count++;
+	kdDebug() << "\tcount = " << m_count << endl;
+	if (m_file[fd])
 	{
-		QFileInfo info(m_pipes[i]);
-		if ( ! info.exists() )
-		{
-			mode_t perms = S_IRUSR | S_IWUSR | S_IRGRP| S_IROTH;
-			//create the dir first
-			if (mkdir(info.dirPath().local8Bit(), perms | S_IXUSR) == -1)
-				perror( "Could not create directory for pipe ");
-	
-			if (mkfifo(m_pipes[i].local8Bit(), perms) == -1)
-				perror( "Could not create pipe ");
-		}
-	}
-}
+		QString line;
+		m_file[fd]->readLine(line, 80);
+		line=line.stripWhiteSpace();
+		kdDebug() << m_count << ":" << line << endl;
 
-void KileLyxServer::removePipes()
-{
-	for (uint i = 0; i < m_pipes.count(); i++)
-	{
-		QFile file(m_pipes[i]);
-		file.remove();
-	}
-}
+		QRegExp cite(":citation-insert:(.*)$");
+		QRegExp bibtexdbadd(":bibtex-database-add:(.*)$");
 
-void KileLyxServer::openPipes()
-{
-	for (uint i = 0; i < m_pipes.count(); i++)
-	{
-		QFile *file = new QFile(m_pipes[i]);
-		if (!file->open(IO_ReadWrite))
-		{
-			kdError() << "Could not open " << m_pipes[i] << endl;
-		}
-		else
-		{
-			kdDebug() << "Opened " << m_pipes[i] << endl;
-			m_pipeFile.append(file);
-			if (m_pipes[i].endsWith(".in")) m_fds.append(file->handle());
-		}
-	}
-}
-
-void KileLyxServer::processLine(const QString &line)
-{
-	static QRegExp::QRegExp reCite(":citation-insert:(.*)$");
-	static QRegExp::QRegExp reBibtexdbadd(":bibtex-database-add:(.*)$");
-
-	if (reCite.search(line) > -1)
-		emit insert(KileAction::TagData("Cite", "\\cite{"+reCite.cap(1)+"}", QString::null, 7+ reCite.cap(1).length()));
-	else if ( reBibtexdbadd.search(line) > -1 )
-		emit insert(KileAction::TagData("BibTeX db add", "\\bibliography{"+ reBibtexdbadd.cap(1) + "}", QString::null, 15+reBibtexdbadd.cap(1).length()));
-}
-
-LyxPipeThread::LyxPipeThread(KileLyxServer *receiver, QValueList<int> fds) : 
-	m_receiver(receiver),
-	m_fds(fds)
-{}
-
-void LyxPipeThread::run()
-{
-	fd_set set;
-	struct timeval timeout;
-
-	/* Initialize the file descriptor set. */
-	FD_ZERO (&set);
-	for (uint i = 0; i < m_fds.count(); i++)
-		FD_SET (m_fds[i], &set);
-
-	/* Initialize the timeout data structure. */
-	timeout.tv_sec = 60;
-	timeout.tv_usec = 0;
-
-	char *result = new char[2];
-	size_t bytesRead = 1;
-	QString line = QString::null;
-
-	while (true)
-	{
-		select (FD_SETSIZE, &set, NULL, NULL, &timeout);
-		for ( uint i = 0; i < m_fds.count(); i++ )
-		{
-			if (FD_ISSET(m_fds[i], &set)) 
-			{
-				bytesRead = read(m_fds[i], result, bytesRead);
-				result[1] = '\0';
-				if (bytesRead > 0) 
-				{
-					if ( result[0] != '\n' )
-						line += QString::fromLocal8Bit(result, (int)bytesRead);
-					else
-					{
-						kdDebug() << "read line : " << line.stripWhiteSpace() << endl;
-						m_receiver->processLine(line); 
-						line = QString::null;
-					}
-				}
-			}
-		}
+		if (cite.search(line) > -1)
+			emit(insert(KileAction::TagData("Cite", "\\cite{"+cite.cap(1)+"}", QString::null, 7+cite.cap(1).length())));
+		else if ( bibtexdbadd.search(line) > -1 )
+			emit(insert(KileAction::TagData("BibTeX db add", "\\bibliography{"+ bibtexdbadd.cap(1) + "}", QString::null, 15+bibtexdbadd.cap(1).length())));
 	}
 }
 
--- kile/kilelyxserver.h
+++ kile/kilelyxserver.h
@@ -25,8 +25,6 @@
 #include <qobject.h>
 #include <qptrlist.h>
 #include <qintdict.h>
-#include <qstringlist.h>
-#include <qthread.h>
 
 /**
  * @short Simple server that impersonates as LyX to work with gBib, pyBibliographer etc.
@@ -51,35 +49,20 @@ public:
 public slots:
 	bool start();
 	void stop();
-	void processLine(const QString &);
 
-private:
-	void openPipes();
-	void createPipes();
-	void removePipes();
+protected slots:
+	void receive(int);
+	bool openPipes();
 
 signals:
 	void insert(const KileAction::TagData &);
 
 private:
-	QValueList<int>					m_fds;
-	bool							m_running;
-	QStringList						m_pipes;
-	QPtrList<QFile>					m_pipeFile;
-	QThread							*m_thread;
-};
-
-class LyxPipeThread : public QThread
-{
-public:
-	LyxPipeThread(KileLyxServer *receiver, QValueList<int> fds);
-	
-protected:
-	virtual void run();
-
-private:
-	KileLyxServer 	*m_receiver;
-	QValueList<int>	m_fds;
+	QPtrList<QFile>					m_pipeIn;
+	QPtrList<QSocketNotifier>			m_notifier;
+	QIntDict<QFile>					m_file;
+	bool								m_running;
+	int								m_count;
 };
 
 #endif // _LYXSERVER_H_

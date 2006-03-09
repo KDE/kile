ec6afdee03f04927f0c951eadc1cc632ce579aad
Jeroen Wijnhout <wijnhout@science.uva.nl>
2006-03-09 17:59:57 +0000

    Added the tarball generation scripts.

---
+++ dist/build_tarball.sh
@@ -0,0 +1,2 @@
+export UNSERMAKE=no
+./createPackage.sh -ab trunk/extragear/office -a kile --i18n-sub extragear-office --admin branches/KDE/3.5/kde-common/admin -av 1.9 --package
---
+++ dist/quick_tarball.sh
@@ -0,0 +1,2 @@
+export UNSERMAKE=no
+./createPackage.sh -ab trunk/extragear/office -a kile --noi18n --admin branches/KDE/3.5/kde-common/admin -av 1.9 --package

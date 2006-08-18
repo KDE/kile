aec2a33c32ca1691543b87e83f8d80eb0854549c
Jeroen Wijnhout <wijnhout@science.uva.nl>
2006-08-18 17:53:33 +0000

    Script for creating the kile-1.9.2 package.

--- dist/build_tarball.sh
+++ dist/build_tarball.sh
@@ -1,2 +1,2 @@
 export UNSERMAKE=no
-./createPackage.sh -ab trunk/extragear/office -a kile --i18n-sub extragear-office --admin branches/KDE/3.5/kde-common/admin -av 1.9 --package
+./createPackage.sh -ab /branches/kile/1.9 -a kile --i18n-sub extragear-office --admin branches/KDE/3.5/kde-common/admin -av 1.9.2 --package

f932db338c7d1479a638f9bcf0773589d9692b2d
Michel Ludwig <michel.ludwig@gmail.com>
2006-10-25 10:46:40 +0000

    Add missing file, sorry for that.

---
+++ kile/kileconstants.h
@@ -0,0 +1,23 @@
+/**************************************************************************
+*   Copyright (C) 2006 by Michel Ludwig (michel.ludwig@kdemail.net)       *
+***************************************************************************/
+
+/**************************************************************************
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************/
+
+#ifndef KILECONSTANTS_H
+#define KILECONSTANTS_H
+
+namespace KileDocument {
+	enum Type {Undefined, Text, LaTeX, BibTeX};
+
+}
+
+
+#endif

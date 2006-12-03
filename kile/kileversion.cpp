ee5319d67a5313d1e34b880dfcae1056db1abcd5
Michel Ludwig <michel.ludwig@gmail.com>
2006-12-03 20:05:44 +0000

    - Increase Kile's version to 2.0.0. - Add a function that allows to compare two version strings.

---
+++ kile/kileversion.cpp
@@ -0,0 +1,43 @@
+/**************************************************************************
+*   Copyright (C) 2006 by Michel Ludwig (michel.ludwig@kdemail.net)       *
+***************************************************************************/
+
+/**************************************************************************
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************/
+
+#include <qstringlist.h>
+
+#include "kileversion.h"
+
+int compareVersionStrings(const QString& s1, const QString& s2) {
+	QStringList l1 = QStringList::split(".", s1);
+	QStringList l2 = QStringList::split(".", s2);
+	while(l1.size() < 3) {
+		l1.push_back("0");
+	}
+	while(l2.size() < 3) {
+		l2.push_back("0");
+	}
+	QStringList::iterator i1 = l1.begin();
+	QStringList::iterator i2 = l2.begin();
+	for(unsigned int i = 0; i < 3; ++i) {
+		unsigned int c1 = (*i1).toUInt();
+		unsigned int c2 = (*i2).toUInt();
+		if(c1 < c2) {
+			return -1;
+		}
+		else if(c1 > c2) {
+			return 1;
+		}
+		++i1;
+		++i2;
+	}
+	return 0;
+}
+
--- kile/kileversion.h
+++ kile/kileversion.h
@@ -19,8 +19,15 @@
 
 #include <qstring.h>
 
-const QString kileVersion="1.9";
+const QString kileVersion="2.0";
+const QString kileFullVersion="2.0.0";
 const QString kilePrVersion="1";
 
+/**
+ * Compares two strings of the form "a.b(.c)" and "d.e(.f)" lexicographically,
+ * where a, b, c, d, e and f are natural numbers.
+ * @return 1 if s1 is bigger than s2, 0 if s1 == s2, -1 if s1 is smaller than s2
+ **/
+int compareVersionStrings(const QString& s1, const QString& s2);
 
 #endif

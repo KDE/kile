ddbb40f9a5939defc46ca0de1b0b288b8a51f6a7
Holger Danielsson <holger.danielsson@versanet.de>
2007-03-18 07:02:49 +0000

    add files for other file extensions than .tex

---
+++ kile/kileextensions.cpp
@@ -0,0 +1,116 @@
+/***************************************************************************
+    begin                : Mar 12 2007
+    copyright            : 2007 by Holger Danielsson
+    email                : holger.danielsson@versanet.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+
+#include <qstring.h>
+#include <qstringlist.h>
+#include <qfileinfo.h>
+
+#include <klocale.h>
+#include <kdebug.h>
+
+#include <kileextensions.h>
+
+namespace KileDocument 
+{
+
+//////////////////// Extensions ////////////////////
+
+Extensions::Extensions()
+{
+	m_documents = ".tex .ltx .latex .dtx .ins";
+	m_packages = ".cls .sty";
+	m_bibtex = ".bib";
+	m_metapost = ".mp";
+	m_script = ".js";
+	//m_images = ".eps .pdf .dvi .ps .fig .gif .jpg .jpeg .png";
+	m_images = ".eps .jpg .jpeg .png .pdf .ps .fig .gif";
+
+	m_documentDefault = ".tex";
+	m_bibtexDefault = ".bib";
+	m_metapostDefault = ".mp";
+	m_scriptDefault = ".js";
+}
+
+//////////////////// file filter ////////////////////
+
+QString Extensions::fileFilter(uint type)
+{
+	QString ext,text;
+	switch ( type )
+	{ 
+		case LATEX_EXT_DOC:
+			ext = m_documents;
+			text = i18n("(La)TeX Source Files");
+			break;
+		case LATEX_EXT_PKG:
+			ext = m_packages;
+			text = i18n("(La)TeX Packages");
+			break;
+		case LATEX_EXT_BIB:
+			ext = m_bibtex;
+			text = i18n("BibTeX Files");
+			break;
+		case LATEX_EXT_MP:
+			ext = m_metapost;
+			text = i18n("Metapost Files");
+			break;
+		case LATEX_EXT_JS:
+			ext = m_script;
+			text = i18n("Kile Script Files");
+			break;
+		default:
+			return QString::null;
+	}
+
+	ext.replace(".","*.");
+	return ext + "|" + text;
+}
+
+//////////////////// document type ////////////////////
+
+bool Extensions::isTexFile(const KURL & url)
+{
+	//TODO use mimetype
+	QString ext = "." + QFileInfo(url.fileName()).extension(false);
+	return isLatexDocument(ext) || isLatexPackage(ext);
+}
+
+bool Extensions::isBibFile(const KURL & url)
+{
+	QString ext = "." + QFileInfo(url.fileName()).extension(false);
+	return isBibtex(ext);
+}
+
+bool Extensions::isScriptFile(const KURL& url)
+{
+	QString ext = "." + QFileInfo(url.fileName()).extension(false);
+	return isScript(ext);
+}
+
+bool Extensions::validExtension(const QString &ext, const QString &extensions)
+{
+	QStringList extlist = QStringList::split(" ",extensions);
+	for ( QStringList::ConstIterator it=extlist.begin(); it!=extlist.end(); ++it )
+	{
+		if ( (*it) == ext ) 
+			return true;
+	}
+
+	return false;
+}
+
+
+}
---
+++ kile/kileextensions.h
@@ -0,0 +1,78 @@
+/***************************************************************************
+    begin                : Mar 12 2007
+    copyright            : 2007 by Holger Danielsson
+    email                : holger.danielsson@versanet.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef KILEEXTENSIONS_H
+#define KILEEXTENSIONS_H
+
+#include <qstring.h>
+#include <qstringlist.h>
+
+#include <kurl.h>
+
+namespace KileDocument 
+{
+
+class Extensions
+{
+public:
+	Extensions();
+	~Extensions() {}
+
+	enum { LATEX_EXT_DOC=1,  LATEX_EXT_PKG=2,  LATEX_EXT_BIB=4, LATEX_EXT_IMG=8,  LATEX_EXT_MP=16, LATEX_EXT_JS=32 };
+
+	QString latexDocuments() { return m_documents; }
+	QString latexPackages() { return m_packages; }
+	QString bibtex() { return m_bibtex; }
+	QString images() { return m_images; }
+	QString metapost() { return m_metapost; }
+
+	QString latexDocumentDefault() { return m_documentDefault; }
+	QString bibtexDefault() { return m_bibtexDefault; }
+	QString metapostDefault() { return m_metapostDefault; }
+
+	QString latexDocumentFileFilter() { return fileFilter(LATEX_EXT_DOC); }
+	QString latexPackageFileFilter() { return fileFilter(LATEX_EXT_PKG); }
+	QString bibtexFileFilter() { return fileFilter(LATEX_EXT_BIB); }
+	QString imageFileFilter() { return fileFilter(LATEX_EXT_IMG); }
+	QString metapostFileFilter() { return fileFilter(LATEX_EXT_MP); }
+	QString scriptFileFilter() { return fileFilter(LATEX_EXT_JS); }
+	
+	bool isTexFile(const KURL &url);
+	bool isBibFile(const KURL &url);
+	bool isScriptFile(const KURL & url);
+
+	bool isLatexDocument(const QString &ext) { return validExtension(ext,m_documents); }
+	bool isLatexPackage(const QString &ext) { return validExtension(ext,m_packages); }
+	bool isImage(const QString &ext) { return validExtension(ext,m_images); }
+
+private:
+	QString m_documents, m_packages;
+	QString m_bibtex, m_metapost;
+	QString m_images, m_script;
+
+	QString m_documentDefault, m_bibtexDefault;
+	QString m_metapostDefault, m_scriptDefault;
+
+	bool isBibtex(const QString &ext) { return validExtension(ext,m_bibtex); }
+	bool isMetapost(const QString &ext) { return validExtension(ext,m_metapost); }
+	bool isScript(const QString &ext) { return validExtension(ext,m_script); }
+	bool validExtension(const QString &ext, const QString &extensions);
+
+	QString fileFilter(uint type);
+};
+
+}
+
+#endif

be20486e23a0b733b8405fba3fb3906e18767f4a
Laurent Montel <montel@kde.org>
2007-11-26 12:09:56 +0000

    Extract message

---
+++ kile/Messages.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env bash
+
+$PREPARETIPS > tips.cpp
+LIST=`find . -name \*.h -o -name \*.hh -o -name \*.H -o -name \*.hxx -o -name \*.hpp -o -name \*.cpp -o -name \*.cc -o -name \*.cxx -o -name \*.ecpp -o -name \*.C`
+if test -n "$LIST"; then
+   $XGETTEXT $LIST -o $(podir)/kile.pot;
+fi
+rm -f tips.cpp

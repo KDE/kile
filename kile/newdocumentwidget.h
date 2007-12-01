44acc11b1f2bf4fd5b122e3a7645abd4b967f9cd
Michel Ludwig <michel.ludwig@gmail.com>
2007-12-01 17:46:31 +0000

    Create header and source file for NewDocumentWidget.

---
+++ kile/newdocumentwidget.h
@@ -0,0 +1,30 @@
+/**************************************************************************
+*   Copyright (C) 2007 by Michel Ludwig (michel.ludwig@kdemail.net)       *
+***************************************************************************/
+
+/**************************************************************************
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************/
+
+#ifndef NEWDOCUMENTWIDGET_H
+#define NEWDOCUMENTWIDGET_H
+
+#include <QWidget>
+
+#include "ui_newdocumentwidget.h"
+
+class NewDocumentWidget : public QWidget, public Ui::NewDocumentWidget
+{
+	Q_OBJECT
+
+	public:
+		NewDocumentWidget(QWidget *parent = 0);
+		~NewDocumentWidget();
+};
+
+#endif

bf7c4a7fb332ca91f35cff97347db9d928a74f83
Michel Ludwig <michel.ludwig@gmail.com>
2007-12-04 21:43:21 +0000

    Create header / implementation files for ConfigCheckerWidget.

---
+++ kile/configcheckerwidget.cpp
@@ -0,0 +1,41 @@
+/**************************************************************************
+*   Copyright (C) 2007 by Michel Ludwig (michel.ludwig@kdemail.net)       *
+***************************************************************************/
+
+/**************************************************************************
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************/
+
+#include "configcheckerwidget.h"
+
+ConfigCheckerWidget::ConfigCheckerWidget(QWidget *parent) : QWidget(parent)
+{
+	setupUi(this);
+}
+
+ConfigCheckerWidget::~ConfigCheckerWidget()
+{
+}
+
+QProgressBar* ConfigCheckerWidget::progressBar()
+{
+	return m_progress;
+}
+
+
+QLabel* ConfigCheckerWidget::label()
+{
+	return m_lbChecking;
+}
+
+K3ListBox* ConfigCheckerWidget::listBox()
+{
+	return m_lstResults;
+}
+
+#include "configcheckerwidget.moc"
---
+++ kile/configcheckerwidget.h
@@ -0,0 +1,39 @@
+/**************************************************************************
+*   Copyright (C) 2007 by Michel Ludwig (michel.ludwig@kdemail.net)       *
+***************************************************************************/
+
+/**************************************************************************
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************/
+
+#ifndef CONFIGCHECKERWIDGET_H
+#define CONFIGCHECKERWIDGET_H
+
+#include <QLabel>
+#include <QProgressBar>
+#include <QWidget>
+
+#include <k3listbox.h>
+
+#include "ui_configcheckerwidget.h"
+
+class ConfigCheckerWidget : public QWidget, public Ui::ConfigCheckerWidget
+{
+	Q_OBJECT
+
+	public:
+		ConfigCheckerWidget(QWidget *parent = 0);
+		~ConfigCheckerWidget();
+
+		QProgressBar* progressBar();
+		QLabel* label();
+		K3ListBox* listBox();
+
+};
+
+#endif

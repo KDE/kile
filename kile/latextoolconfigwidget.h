0307ac7a83f31c545f55b6260336a162c952ca24
Michel Ludwig <michel.ludwig@gmail.com>
2007-12-13 22:11:54 +0000

    Create header / implementation files for the remaining (?) .ui files.

---
+++ kile/latextoolconfigwidget.h
@@ -0,0 +1,30 @@
+/**************************************************************************
+*   Copyright (C) 2007 by Michel Ludwig (michel.ludwig@kdemail.net)       *
+***************************************************************************/
+
+/**************************************************************************
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************/
+
+#ifndef LATEXTOOLCONFIGWIDGET_H
+#define LATEXTOOLCONFIGWIDGET_H
+
+#include <QWidget>
+
+#include "ui_latextoolconfigwidget.h"
+
+class LaTeXToolConfigWidget : public QWidget, public Ui::LaTeXToolConfigWidget
+{
+	Q_OBJECT
+
+	public:
+		LaTeXToolConfigWidget(QWidget *parent = 0);
+		~LaTeXToolConfigWidget();
+};
+
+#endif
---
+++ kile/processtoolconfigwidget.h
@@ -0,0 +1,30 @@
+/**************************************************************************
+*   Copyright (C) 2007 by Michel Ludwig (michel.ludwig@kdemail.net)       *
+***************************************************************************/
+
+/**************************************************************************
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************/
+
+#ifndef PROCESSTOOLCONFIGWIDGET_H
+#define PROCESSTOOLCONFIGWIDGET_H
+
+#include <QWidget>
+
+#include "ui_processtoolconfigwidget.h"
+
+class ProcessToolConfigWidget : public QWidget, public Ui::ProcessToolConfigWidget
+{
+	Q_OBJECT
+
+	public:
+		ProcessToolConfigWidget(QWidget *parent = 0);
+		~ProcessToolConfigWidget();
+};
+
+#endif

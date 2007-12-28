c2cea5ccb8973de1b1000c90e3a7de7e229d4782
Michel Ludwig <michel.ludwig@gmail.com>
2007-12-28 16:46:08 +0000

    Sync kile project on disk before archiving, thanks to Alfredo Pironti for reporting this issue (forward port of 746391).

--- src/kiletool.cpp
+++ src/kiletool.cpp
@@ -578,7 +578,8 @@ namespace KileTool
 			Base::setSource(source);
 			return;
 		}
-		
+
+		manager()->info()->docManager()->projectSave(m_project);
 		Base::setSource(m_project->url().path());
 		m_fileList = m_project->archiveFileList();
 		

c0c3f5b34b44d71f0415b215a3ba78050c0801de
Michel Ludwig <michel.ludwig@gmail.com>
2008-01-27 18:16:49 +0000

    Add licence and copyright information.

--- src/dialogs/newtabulardialog.cpp
+++ src/dialogs/newtabulardialog.cpp
@@ -1,3 +1,17 @@
+/********************************************************************************************
+    begin                : Sunday Jun 27 2008
+    copyright            : (C) 2008 by Mathias Soeken (msoeken@informatik.uni-bremen.de)
+ ********************************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #include "newtabulardialog.h"
 
 #include <QHBoxLayout>

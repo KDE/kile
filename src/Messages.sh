226f3d3089b19b1c15155926c7c4cf35ac621d00
Albert Astals Cid <tsdgeos@terra.es>
2008-07-02 18:18:21 +0000

    MakeItWork

--- src/Messages.sh
+++ src/Messages.sh
@@ -3,6 +3,6 @@
 $PREPARETIPS > tips.cpp
 LIST=`find . -name \*.h -o -name \*.hh -o -name \*.H -o -name \*.hxx -o -name \*.hpp -o -name \*.cpp -o -name \*.cc -o -name \*.cxx -o -name \*.ecpp -o -name \*.C`
 if test -n "$LIST"; then
-   $XGETTEXT $LIST -o $(podir)/kile.pot;
+   $XGETTEXT $LIST -o $podir/kile.pot;
 fi
 rm -f tips.cpp

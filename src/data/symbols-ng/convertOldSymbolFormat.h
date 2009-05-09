3643da49ce71c2ccf25f544bca7241b5e7607e8f
Thomas Braun <thomas.braun@virtuell-zuhause.de>
2009-05-09 20:51:17 +0000

    add converter script from old tex symbol format to new format works already quite well.
    To convert a tex file do:
    - make convertOldSymbolFormat
    - cp template.xml mySymbols.xml
    - ./convertOldSymbolFormat mysymbols.tex >> mySymbols.xml
    
    Then check the file and then call
     ./gesymb-ng mySymbols.xml
    
    Of course you can also add Unicode Commands and/or Comments.

---
+++ src/data/symbols-ng/convertOldSymbolFormat.h
@@ -0,0 +1,35 @@
+/***************************************************************************
+begin                : Sat Mai 9 2009
+copyright            : (C) 2009 by Thomas Braun
+email                : thomas.braun@virtuell-zuhause.de
+***************************************************************************/
+
+/***************************************************************************
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************/
+
+#ifndef GESYMB_H
+#define GESYMB_H
+
+#include <QImage>
+#include <QString>
+#include <QRegExp>
+#include <QFile>
+#include <QTextStream>
+#include <QTextDocument>
+#include <QStringList>
+
+#include <stdlib.h>
+#include <iostream>
+#include "../../symbolviewclasses.h"
+
+void usage();
+void outputXML(const QString ,const QString , QList< Package >& packages, bool mathMode);
+void extractPackageString(const QString&string, QList<Package> &packages);
+
+#endif

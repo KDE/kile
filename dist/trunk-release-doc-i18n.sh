a8817db472e157db7f9602a02e9d90bcc9d0197d
Thomas Braun <thomas.braun@virtuell-zuhause.de>
2009-06-08 19:05:13 +0000

    change the script to also work with non svn working copys, in this case use the current date as version string this is for our git users (and especially for me)

--- dist/trunk-release-doc-i18n.sh
+++ dist/trunk-release-doc-i18n.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 # some changes for KDE4
 
-svn up # get newest revision info
-revision=`LANG=C svn info | grep "Last Changed Rev:" | tr -d " " | cut -f 2 -d":"`
-logfile=createPackage-`date +%S-%M-%H--%d.%m.%y`.log
+date=`date +%S-%M-%H--%d.%m.%y`
+logfile=createPackage-$date.log
+
+if [ -e ".svn" ]; then
+	svn up # get newest revision info
+	version=r`LANG=C svn info | grep "Last Changed Rev:" | tr -d " " | cut -f 2 -d":"`
+else
+	version=$date
+fi
 
 ./createPackage.sh -ab trunk/extragear/office -a kile --i18n-base trunk/l10n-kde4 \
---i18n-sub extragear-office --notoplevel --package --logfile $logfile -av trunk-r$revision
+--i18n-sub extragear-office --notoplevel --package --logfile $logfile -av trunk-$version

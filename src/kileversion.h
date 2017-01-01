9ca83c8e1ae0d58ace35fb50f43c2e2c714f2f96
Michel Ludwig <michel.ludwig@kdemail.net>
2017-01-01 12:21:47 +0100

    Raise overall copyright year to 2017

--- src/kileversion.h
+++ src/kileversion.h
@@ -1,7 +1,7 @@
 /****************************************************************************************
     begin                : Wednesday Jan 25 2006
     copyright            : (C) 2006 by Thomas Braun (thomas.braun@virtuell-zuhause.de)
-                               2011-2016 by Michel Ludwig (michel.ludwig@kdemail.net)
+                               2011-2017 by Michel Ludwig (michel.ludwig@kdemail.net)
  ****************************************************************************************/
 
 /***************************************************************************
@@ -25,7 +25,7 @@ const QString kileFullVersion="2.9.60"; // only use numbers and '.'
 #define KILERC_VERSION 8
 
 // the last-copyright-year is used in 'main.cpp' and 'CMakeLists.txt'
-#define KILE_LAST_COPYRIGHT_YEAR 2016
+#define KILE_LAST_COPYRIGHT_YEAR 2017
 
 /**
  * Compares two strings of the form "a.b(.c)" and "d.e(.f)" lexicographically,

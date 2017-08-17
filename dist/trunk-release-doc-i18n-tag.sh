c4f55232efe164c0cf60d1e694a5c9d7acdb8721
Michel Ludwig <michel.ludwig@kdemail.net>
2017-08-17 21:17:28 +0200

    Remove CMake release code & release scripts that are obsolete now for KF5

--- dist/trunk-release-doc-i18n-tag.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ -z "$1" ]; then
-    echo "Usage: $0 <version>"
-    exit 1
-fi
-
-./createPackage.sh --svnroot "svn://anonsvn.kde.org/home/kde" -ab trunk/extragear/office -a kile --i18n-base trunk/l10n-kde4 \
---i18n-sub extragear-office --package --logfile $0.log -av $1

KDE/kile

commit 06f319ed0cdb2671c3c51e22bd0b806f43e0caa0

Introduce an abbreviation manager that handles abbreviations and their replacements.

src/widgets/abbreviationview.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************************
     begin                : Feb 24 2007
     copyright            : 2007 by Holger Danielsson (holger.danielsson@versanet.de)
-                           2008 by Michel Ludwig (michel.ludwig@kdemail.net)
+                           2008 - 2009 by Michel Ludwig (michel.ludwig@kdemail.net)
 *****************************************************************************************/
 
 /***************************************************************************
@@ -24,16 +24,15 @@
 #include <KMenu>
 #include <KMessageBox>
 
+#include "abbreviationmanager.h"
 #include "dialogs/abbreviationinputdialog.h"
-
 #include "kiledebug.h"
 
 namespace KileWidget {
 
-AbbreviationView::AbbreviationView(QWidget *parent, const char *name)
-	: QTreeWidget(parent), m_changes(false)
+AbbreviationView::AbbreviationView(KileAbbreviation::Manager *manager, QWidget *parent)
+	: QTreeWidget(parent), m_abbreviationManager(manager)
 {
-	setObjectName(name);
 	setColumnCount(2);
 	QStringList headerLabelList;
 	headerLabelList << i18n("Short") << QString() << i18n("Expanded Text");
@@ -55,70 +54,25 @@ AbbreviationView::~AbbreviationView()
 //////////////////// init abbreviation view with wordlists ////////////////////
 
 
-void AbbreviationView::init(const QStringList *globallist, const QStringList *locallist)
+void AbbreviationView::updateAbbreviations()
 {
+KILE_DEBUG();
 	setUpdatesEnabled(false);
 	clear();
-	addWordlist(globallist,true);
-	addWordlist(locallist,false);
+	const QMap<QString, QPair<QString, bool> >& abbreviationMap = m_abbreviationManager->getAbbreviationMap();
+	QList<QTreeWidgetItem*> itemList;
+	for(QMap<QString, QPair<QString, bool> >::const_iterator i = abbreviationMap.begin();
+	    i != abbreviationMap.end(); ++i) {
+		QPair<QString, bool> pair = i.value();
+		QTreeWidgetItem *item = new QTreeWidgetItem();
+		item->setText(ALVabbrev, i.key());
+		item->setText(ALVlocal, (pair.second) ? QString() : "*");
+		item->setText(ALVexpansion, pair.first);
+		itemList.push_back(item);
+	}
+	addTopLevelItems(itemList);
+
 	setUpdatesEnabled(true);
-
-	m_changes = false;
-}
-
-void AbbreviationView::addWordlist(const QStringList *wordlist, bool global)
-{
-	QString type = (global) ? QString() : "*";
-
-	QStringList::ConstIterator it;
-	for(it = wordlist->begin(); it != wordlist->end(); ++it) {
-		int index = (*it).indexOf('=');
-		if(index >= 0) {
-			QTreeWidgetItem *item = new QTreeWidgetItem(this);
-			item->setText(0, (*it).left(index));
-			item->setText(1, type);
-			item->setText(2, (*it).right((*it).length() - index - 1));
-			addTopLevelItem(item);
-		}
-	}
-}
-
-//////////////////// save local abbreviation list ////////////////////
-
-void AbbreviationView::saveLocalAbbreviation(const QString &filename)
-{
-	if(!m_changes) {
-		return;
-	}
-
-	KILE_DEBUG() << "=== AbbreviationView::saveLocalAbbreviation ===================";
-	// create the file 
-	QFile abbrevfile(filename);
-	if(!abbrevfile.open( QIODevice::WriteOnly)) {
-		return;
-	}
-
-	QTextStream stream(&abbrevfile);
-	stream << "# abbreviation mode: editable abbreviations\n";
-	stream << "# dani/2007\n";
-
-	//QTextCodec *codec = QTextCodec::codecForName(m_ki->activeTextDocument()->encoding().ascii());
-	// stream.setCodec(codec);
-
-	QTreeWidgetItemIterator it(this);
-	while(*it) {
-		QTreeWidgetItem *current = *it;
-		if(current->text(AbbreviationView::ALVlocal) == "*") {
-			stream << current->text(AbbreviationView::ALVabbrev)
-			       << "=" 
-			       << current->text(AbbreviationView::ALVexpansion)
-			       << "\n";
-		}
-		++it;
-	}
-	abbrevfile.close();
-
-	m_changes = false;
 }
 
 //////////////////// find abbreviation ////////////////////
@@ -180,7 +134,7 @@ void AbbreviationView::slotAddAbbreviation()
 	if(dialog.exec() == QDialog::Accepted) {
 		QString abbrev, expansion;
 		dialog.abbreviation(abbrev, expansion);
-		addAbbreviation(abbrev, expansion);
+		m_abbreviationManager->updateLocalAbbreviation(abbrev, expansion);
 	}
 }
 
@@ -191,11 +145,16 @@ void AbbreviationView::slotChangeAbbreviation()
 		return;
 	}
 	QTreeWidgetItem *selectedItem = selectedList.first();
+	QString oldAbbreviationText = selectedItem->text(ALVabbrev);
+	QString oldAbbreviationExpansion = selectedItem->text(ALVexpansion);
 	KileDialog::AbbreviationInputDialog dialog(this, selectedItem, ALVedit);
 	if(dialog.exec() == QDialog::Accepted) {
 		QString abbrev, expansion;
 		dialog.abbreviation(abbrev, expansion);
-		changeAbbreviation(selectedItem, abbrev, expansion);
+		if(oldAbbreviationText != abbrev) {
+			m_abbreviationManager->removeLocalAbbreviation(oldAbbreviationText);
+		}
+		m_abbreviationManager->updateLocalAbbreviation(abbrev, expansion);
 	}
 }
 
@@ -205,49 +164,18 @@ void AbbreviationView::slotDeleteAbbreviation()
 	if(selectedList.count() == 0) {
 		return;
 	}
-	deleteAbbreviation(selectedList.first());
-}
-
-void AbbreviationView::addAbbreviation(const QString &abbrev, const QString &expansion)
-{
-	QTreeWidgetItem *item = new QTreeWidgetItem(this);
-	item->setText(0, abbrev);
-	item->setText(1, "*");
-	item->setText(2, expansion);
-	addTopLevelItem(item);
-	QString newAbbrev = abbrev + '=' + expansion;
-
-	emit(updateAbbrevList(QString(), newAbbrev));
-	m_changes = true;
-}
-
-void AbbreviationView::changeAbbreviation(QTreeWidgetItem *item, const QString &abbrev, const QString &expansion)
-{
-	if(item) {
-		QString oldAbbrev = item->text(ALVabbrev) + '=' + item->text(ALVexpansion);
-		QString newAbbrev = abbrev + '=' + expansion;
-		item->setText(ALVabbrev,abbrev);
-		item->setText(ALVexpansion,expansion);
-
-		emit(updateAbbrevList(oldAbbrev,newAbbrev));
-		m_changes = true;
-	}
-}
-
-void AbbreviationView::deleteAbbreviation(QTreeWidgetItem *item)
-{
-	QString abbrev = item->text(ALVabbrev);
-	QString message = i18n("Delete the abbreviation '%1'?", abbrev);
+	QTreeWidgetItem *item = selectedList.first();
+	QString abbreviationText = item->text(ALVabbrev);
+	QString abbreviationExpansion = item->text(ALVexpansion);
+	QString message = i18n("Delete the abbreviation '%1'?", abbreviationText);
 	if(KMessageBox::questionYesNo(this,
 		       "<center>" + message + "</center>",
 		       i18n("Delete Abbreviation") ) == KMessageBox::Yes) {
-		QString s = item->text(ALVabbrev) + '=' + item->text(ALVexpansion);
-		delete item;
-
-		emit(updateAbbrevList(s, QString()));
-		m_changes = true;
+		QString s = abbreviationText + '=' + abbreviationExpansion;
 	}
+	m_abbreviationManager->removeLocalAbbreviation(abbreviationText);
 }
+
 
 }
 

src/CMakeLists.txt
@@ -97,6 +97,7 @@ SET(kile_SRCS
 	dialogs/tabular/tabulartable.cpp
 	configurationmanager.cpp
 	utilities.cpp
+	abbreviationmanager.cpp
 )
 
 SET( kile_UI

src/abbreviationmanager.cpp
@@ -1,0 +1,143 @@
+/********************************************************************************
+*   Copyright (C) 2007 by Holger Danielsson (holger.danielsson@versanet.de)     *
+*                 2008 - 2009 by Michel Ludwig (michel.ludwig@kdemail.net)      *
+*********************************************************************************/
+
+/**************************************************************************
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************/
+
+#include "abbreviationmanager.h"
+
+#include <KMessageBox>
+#include <KStandardDirs>
+
+#include "codecompletion.h"
+#include "kileinfo.h"
+
+namespace KileAbbreviation {
+
+Manager::Manager(KileInfo* kileInfo, QObject *parent) : QObject(parent), m_kileInfo(kileInfo), m_abbreviationsDirty(false)
+{
+	setObjectName("KileAbbreviation::Manager");
+	m_localAbbreviationFile = KStandardDirs::locateLocal("appdata", "complete/abbreviation/", true) + "kile-abbrevs.cwl";
+}
+
+Manager::~Manager()
+{
+}
+
+const AbbreviationMap& Manager::getAbbreviationMap()
+{
+	return m_abbreviationMap;
+}
+
+void Manager::updateLocalAbbreviation(const QString& text, const QString& replacement)
+{
+	if(text.isEmpty() || replacement.isEmpty()) {
+		return;
+	}
+	AbbreviationMap::iterator it = m_abbreviationMap.find(text);
+	if(it != m_abbreviationMap.end()) {
+		StringBooleanPair pair = it.value();
+		if(pair.first == replacement) {
+			return;
+		}
+		m_abbreviationMap.erase(it);
+	}
+	m_abbreviationMap[text] = createLocalAbbreviationPair(replacement);
+	m_abbreviationsDirty = true;
+	emit(abbreviationsChanged());
+}
+
+void Manager::removeLocalAbbreviation(const QString& text)
+{
+	AbbreviationMap::iterator it = m_abbreviationMap.find(text);
+	if(it == m_abbreviationMap.end()) {
+		return;
+	}
+	StringBooleanPair pair = it.value();
+	if(isLocalAbbreviation(pair)) {
+		m_abbreviationMap.erase(it);
+		m_abbreviationsDirty = true;
+	}
+	emit(abbreviationsChanged());
+}
+
+void Manager::readAbbreviationFiles()
+{
+	if(m_abbreviationsDirty) {
+		saveLocalAbbreviations();
+	}
+	m_abbreviationMap.clear();
+	QStringList list = m_kileInfo->codeCompletionManager()->readCWLFiles(KileConfig::completeAbbrev(), "abbreviation");
+	addAbbreviationListToMap(list, true);
+
+	// read local wordlist
+	list = m_kileInfo->codeCompletionManager()->readCWLFile(m_localAbbreviationFile, true);
+	addAbbreviationListToMap(list, false);
+
+	emit(abbreviationsChanged());
+}
+
+void Manager::saveLocalAbbreviations()
+{
+	if(!m_abbreviationsDirty) {
+		return;
+	}
+
+	KILE_DEBUG();
+	// create the file 
+	QFile abbreviationFile(m_localAbbreviationFile);
+	if(!abbreviationFile.open(QIODevice::WriteOnly)) {
+		KMessageBox::error(m_kileInfo->mainWindow(), i18n("Could not save the local abbreviation list!\nError code %i", abbreviationFile.error()),
+		                   i18n("Saving Problem"));
+		return;
+	}
+
+	QTextStream stream(&abbreviationFile);
+	stream << "# abbreviation mode: editable abbreviations\n";
+
+	//QTextCodec *codec = QTextCodec::codecForName(m_ki->activeTextDocument()->encoding().ascii());
+	// stream.setCodec(codec);
+
+	for(AbbreviationMap::iterator i = m_abbreviationMap.begin();
+	    i != m_abbreviationMap.end(); ++i) {
+		StringBooleanPair pair = i.value();
+		if(!pair.second) {
+			stream << QString(i.key()).replace("=", "\\=")
+			       << '=' << pair.first << '\n';
+		}
+	}
+	abbreviationFile.close();
+
+	m_abbreviationsDirty = false;
+}
+
+void Manager::addAbbreviationListToMap(const QStringList& list, bool global)
+{
+	// a '=' symbol in the left-hand side is encoded by '\='
+	for(QStringList::const_iterator i = list.begin(); i != list.end(); ++i) {
+		QString entry = *i;
+		int delimiter = entry.indexOf(QRegExp("[^\\\\]="));
+		if(delimiter < 0) {
+			continue;
+		}
+		QString left = entry.left(delimiter + 1); // [^\\\\]= has length 2.
+		left.replace("\\=", "=");
+		QString right = entry.mid(delimiter + 2); // [^\\\\]= has length 2.
+		if(right.isEmpty()) {
+			continue;
+		}
+		m_abbreviationMap[left] = StringBooleanPair(right, global);
+	}
+}
+
+}
+
+#include "abbreviationmanager.moc"

src/abbreviationmanager.h
@@ -1,0 +1,88 @@
+/**************************************************************************
+*   Copyright (C) 2009 by Michel Ludwig (michel.ludwig@kdemail.net)       *
+***************************************************************************/
+
+/**************************************************************************
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************/
+
+#ifndef ABBREVIATIONMANAGER_H
+#define ABBREVIATIONMANAGER_H
+
+#include <QMap>
+#include <QObject>
+#include <QPair>
+#include <QString>
+
+#include <KConfig>
+
+class KileInfo;
+
+namespace KileAbbreviation {
+
+	typedef QPair<QString, bool> StringBooleanPair;
+	typedef QMap<QString, StringBooleanPair> AbbreviationMap;
+
+	/**
+	 * This manager class is responsible for handling abbreviations.
+	 **/
+	class Manager : public QObject {
+		Q_OBJECT
+
+		public:
+			/**
+			 * Constructs a new manager object.
+			 **/
+			explicit Manager(KileInfo* kileInfo, QObject *parent = NULL);
+			virtual ~Manager();
+
+			// the boolean value is 'true' iff the abbreviation is global
+			const AbbreviationMap& getAbbreviationMap();
+
+			void readAbbreviationFiles();
+			void saveLocalAbbreviations();
+
+			void updateLocalAbbreviation(const QString& text, const QString& replacement);
+			void removeLocalAbbreviation(const QString& text);
+
+			static inline bool isLocalAbbreviation(const StringBooleanPair& p)
+			{
+				return !p.second;
+			}
+
+			static inline bool isGlobalAbbreviation(const StringBooleanPair& p)
+			{
+				return p.second;
+			}
+
+			static inline StringBooleanPair createGlobalAbbreviationPair(const QString& s)
+			{
+				return StringBooleanPair(s, true);
+			}
+
+			static inline StringBooleanPair createLocalAbbreviationPair(const QString& s)
+			{
+				return StringBooleanPair(s, false);
+			}
+
+		Q_SIGNALS:
+			void abbreviationsChanged();
+
+		protected:
+			KileInfo *m_kileInfo;
+			bool m_abbreviationsDirty;
+			QString m_localAbbreviationFile;
+			AbbreviationMap m_abbreviationMap;
+
+			void addAbbreviationListToMap(const QStringList& list, bool global);
+	};
+
+
+}
+
+#endif

src/codecompletion.cpp
@@ -591,14 +591,12 @@ void Manager::readConfig(KConfig *config)
 		// wordlists for Tex/Latex mode
 		QStringList files = KileConfig::completeTex();
 		m_texWordList = readCWLFiles(files, "tex");
+		addUserDefinedLaTeXCommands(m_texWordList);
 
 		// wordlist for dictionary mode
 		files = KileConfig::completeDict();
 		m_dictWordList = readCWLFiles(files, "dictionary");
-
-		// wordlist for abbreviation mode
-		files = KileConfig::completeAbbrev();
-		m_abbrevWordList = readCWLFiles(files, "abbreviation");
+		m_dictWordList.sort();
 
 		// remember changed lists
 		// FIXME: remove these hacks
@@ -725,11 +723,12 @@ void Manager::addUserDefinedLaTeXCommands(QStringList &wordlist)
 	}
 }
 
-void Manager::readCWLFile(QStringList &wordlist, const QString &filename)
+QStringList Manager::readCWLFile(const QString &filename, bool fullPathGiven)
 {
-	QString file = KGlobal::dirs()->findResource("appdata", "complete/" + filename);
+	QStringList toReturn;
+	QString file = fullPathGiven ? filename : KGlobal::dirs()->findResource("appdata", "complete/" + filename);
 	if(file.isEmpty()) {
-		return;
+		return toReturn;
 	}
 
 	QFile f(file);
@@ -738,11 +737,12 @@ void Manager::readCWLFile(QStringList &wordlist, const QString &filename)
 		while(!t.atEnd()) {        // until end of file...
 			QString s = t.readLine().trimmed();       // line of text excluding '\n'
 			if(!(s.isEmpty() || s.at(0) == '#')) {
-				wordlist.append(s);
+				toReturn.append(s);
 			}
 		}
 		f.close();
 	}
+	return toReturn;
 }
 
 QStringList Manager::readCWLFiles(const QStringList &files, const QString &dir)
@@ -753,38 +753,9 @@ QStringList Manager::readCWLFiles(const QStringList &files, const QString &dir)
 	for(int i = 0; i < files.count(); ++i) {
 		// if checked, the wordlist has to be read
 		if(files[i].at(0) == '1') {
-			readCWLFile(wordlist, dir + '/' + files[i].right( files[i].length()-2 ) + ".cwl");
+			wordlist += readCWLFile(dir + '/' + files[i].right( files[i].length()-2 ) + ".cwl");
 		}
 	}
-
-//FIXME: better move the following out of this method
-	// add user defined commands and environments
-	if(dir == "tex") {
-		addUserDefinedLaTeXCommands(wordlist);
-	}
-	else if(dir == "dictionary") {
-		wordlist.sort();
-	}
-	else if(dir == "abbreviation") {
-// FIXME
-/*
-		// read local wordlist
-		QStringList localWordlist;
-		readWordlist(localWordlist, QString(), false);
-
-		// add local/global wordlists to the abbreviation view
-		m_abbrevListview->init(&wordlist,&localWordlist);
-
-		// finally add local wordlists to the abbreviation list
-		QStringList::ConstIterator it;
-		for(it = localWordlist.begin(); it != localWordlist.end(); ++it) {
-			wordlist.append(*it);
-		}
-
-		wordlist.sort();
-*/
-	}
-
 	return wordlist;
 }
 
@@ -814,7 +785,6 @@ namespace KileDocument
 
 		// local abbreviation list
 		m_abbrevListview = NULL;
-		m_localAbbrevFile = KStandardDirs::locateLocal("appdata", "complete/abbreviation/", true) + "kile-abbrevs.cwl";
 
 		//reRef.setPattern("^\\\\(pageref|ref|xyz)\\{");
 		m_completeTimer = new QTimer( this );
@@ -917,15 +887,6 @@ namespace KileDocument
 		KConfigGroup configGroup = config->group("Kate Document Defaults");
 		m_autobrackets = ( configGroup.readEntry("Basic Config Flags",0) & cfAutoBrackets );
 		m_autoindent   = ( configGroup.readEntry("Indentation Mode",0) > 0 );
-	}
-
-	// save local abbreviation changes
-	// (for example before a new configuration should be read)
-
-	void CodeCompletion::saveLocalChanges()
-	{
-		KILE_DEBUG() << "=== CodeCompletion::saveLocalChanges ===================";
-		m_abbrevListview->saveLocalAbbreviation(m_localAbbrevFile);
 	}
 
 	//////////////////// references and citations ////////////////////

src/codecompletion.h
@@ -105,6 +105,9 @@ namespace KileCodeCompletion
 
 			void readConfig(KConfig *config);
 
+			QStringList readCWLFile(const QString &filename, bool fullPathGiven = false);
+			QStringList readCWLFiles(const QStringList &files, const QString &dir);
+
 		public Q_SLOTS:
 			void startLaTeXCompletion(KTextEditor::View *view = NULL);
 			void startLaTeXEnvironment(KTextEditor::View *view = NULL);
@@ -119,8 +122,6 @@ namespace KileCodeCompletion
 			QRegExp m_citeExtRegExp;
 	
 			void addUserDefinedLaTeXCommands(QStringList &wordlist);
-			void readCWLFile(QStringList &wordlist, const QString &filename);
-			QStringList readCWLFiles(const QStringList &files, const QString &dir);
 			void buildReferenceCitationRegularExpressions();
 			QString getCommandsString(KileDocument::CmdAttribute attrtype);
 }	;
@@ -171,7 +172,6 @@ public:
 
 	void readConfig(KConfig *config);
 	void readKateConfigFlags(KConfig *config);
-	void saveLocalChanges();
 
 	void setAbbreviationListview(KileWidget::AbbreviationView *listview);
 

src/kile.cpp
@@ -44,6 +44,7 @@
 #include <KXmlGuiWindow>
 #include <KSelectAction>
 
+#include "abbreviationmanager.h"
 #include "configurationmanager.h"
 #include "documentinfo.h"
 #include "kileactions.h"
@@ -549,14 +550,11 @@ void Kile::setupSymbolViews()
 
 void Kile::setupAbbreviationView()
 {
-	m_kileAbbrevView = new KileWidget::AbbreviationView(m_sideBar);
-#ifdef __GNUC__
-#warning Check this.
-#endif
-// 	m_edit->complete()->setAbbreviationListview(m_kileAbbrevView);
+	m_kileAbbrevView = new KileWidget::AbbreviationView(abbreviationManager(), m_sideBar);
+	connect(abbreviationManager(), SIGNAL(abbreviationsChanged()), m_kileAbbrevView, SLOT(updateAbbreviations()));;
 	m_sideBar->addPage(m_kileAbbrevView, SmallIcon("complete3"), i18n("Abbreviation"));
 
-	connect(m_kileAbbrevView, SIGNAL(sendText(const QString& )), this, SLOT(insertText(const QString& )));
+	connect(m_kileAbbrevView, SIGNAL(sendText(const QString&)), this, SLOT(insertText(const QString&)));
 }
 
 void Kile::setupBottomBar()
@@ -2348,7 +2346,7 @@ void Kile::readConfig()
 	else {
 		disableSymbolViewMFUS();
 	}
-
+	abbreviationManager()->readAbbreviationFiles();
 }
 
 void Kile::saveSettings()
@@ -2438,6 +2436,8 @@ void Kile::saveSettings()
 	KileConfig::setBottomBarIndex(m_bottomBar->currentTab());
 
 	KileConfig::setSelectedLeftView(m_sideBar->currentTab());
+
+	abbreviationManager()->saveLocalAbbreviations();
 
 	KileConfig::self()->writeConfig();
 	m_config->sync();

src/kileinfo.cpp
@@ -35,6 +35,7 @@
 #include "kileproject.h"
 #include "kileuntitled.h"
 #include "scriptmanager.h"
+#include "abbreviationmanager.h"
 #include "editorkeysequencemanager.h"
 #include "templates.h"
 
@@ -59,6 +60,7 @@ KileInfo::KileInfo(QObject *parent) :
 	                 SIGNAL(documentModificationStatusChanged(KTextEditor::Document*, bool, KTextEditor::ModificationInterface::ModifiedOnDiskReason)),
 	                 m_viewManager,
 	                 SLOT(reflectDocumentModificationStatus(KTextEditor::Document*, bool, KTextEditor::ModificationInterface::ModifiedOnDiskReason)));
+	m_abbreviationManager = new KileAbbreviation::Manager(this, parent);
 }
 
 KileInfo::~KileInfo()

commit ed433a689d9a917307b4eb3e0bc3d77f3ea579b4

Respect user feedback in AbbreviationView::slotDeleteAbbreviation()
The user has to confirm the deletion of an abbreviation. Regardless
of the answer, the abbreviation is always deleted.
This regression was introduced in 06f319ed0.
Triggered by unused variable warning from Clazy.

src/widgets/abbreviationview.cpp
@@ -168,16 +168,14 @@ void AbbreviationView::slotDeleteAbbreviation()
     }
     QTreeWidgetItem *item = selectedList.first();
     QString abbreviationText = item->text(ALVabbrev);
-    QString abbreviationExpansion = item->text(ALVexpansion);
     QString message = i18n("Delete the abbreviation '%1'?", abbreviationText);
     if(KMessageBox::questionTwoActions(this,
                                   "<center>" + message + "</center>",
                                   i18n("Delete Abbreviation"),
                                   KStandardGuiItem::del(),
                                   KStandardGuiItem::cancel()) == KMessageBox::PrimaryAction) {
-        QString s = abbreviationText + '=' + abbreviationExpansion;
+        m_abbreviationManager->removeLocalAbbreviation(abbreviationText);
     }
-    m_abbreviationManager->removeLocalAbbreviation(abbreviationText);
 }
 
 
f0862331dd316a499e8f9ddc4d1d4906a78ca6e2
Simon Martin <simon@nasilyan.com>
2026-08-15 09:47:02 +0200

    Update copyright year.
    Signed-off-by: Simon Martin <simon@nasilyan.com>

--- src/kileversion.h
+++ src/kileversion.h
@@ -26,7 +26,7 @@
 
 // the last-copyright-year is used in 'main.cpp' and 'CMakeLists.txt'
 // KILE_LAST_COPYRIGHT_YEAR must be a string (it may appear as, e.g., 2,019 on Windows otherwise)
-#define KILE_LAST_COPYRIGHT_YEAR "2024"
+#define KILE_LAST_COPYRIGHT_YEAR "2026"
 
 /**
  * Compares two strings of the form "a.b(.c)" and "d.e(.f)" lexicographically,
